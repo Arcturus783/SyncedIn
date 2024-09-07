a22d5f5d2f644f76d46c43dd754ff928235ec19b
anon <arisingh430@gmail.com>
2024-09-06 22:41:22 -0500

    a

---
+++ lib/home.dart
@@ -0,0 +1,66 @@
+//import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+//import 'package:oauth1/oauth1.dart' as auth;
+/*
+IMPORTANT - Here are the fonts we're using.
+Title/App Name: Rubik
+Secondary Headings (Like on the app bar): Figtree
+Paragraph/"Standard text": Ubuntu
+*/
+
+void main() {
+  runApp(const Central());
+}
+
+class Central extends StatelessWidget {
+  const Central({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Home Screen',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 175, 20, 210),
+          title: Text('Insert App Name',
+            style: GoogleFonts.figtree(
+              textStyle: const TextStyle(
+                color: Color.fromARGB(255, 241, 241, 241)))),
+        ),
+        backgroundColor: Colors.transparent,
+        //VVV MAIN CODE IS BELOW VVV
+        body: const Text("Hello World!"),
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,9 @@
-/*
-import 'package:flutter/material.dart';
-import 'package:oauth1/oauth1.dart' as auth;
-import 'package:flutter_gradient_animation_text/flutter_gradient_animation_text.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Schoology API Test',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 8, 16, 137)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: "Demo"),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            GradientAnimationText(
-              text: Text('Welcome!',
-                  style: GoogleFonts.rubik(
-                      textStyle: const TextStyle(
-                    fontSize: 50,
-                  ))),
-              colors: const [
-                Color.fromARGB(255, 41, 0, 72),
-                Color.fromARGB(255, 81, 11, 147),
-                Color.fromARGB(255, 0, 12, 146),
-                Color.fromARGB(255, 0, 0, 255),
-                Color.fromARGB(255, 25, 200, 255),
-                Color.fromARGB(255, 0, 0, 255),
-                Color.fromARGB(255, 0, 12, 146),
-                Color.fromARGB(255, 81, 11, 147),
-              ],
-              duration: const Duration(seconds: 8),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-
-*/
-
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 //import 'package:http/http.dart' as http;
 import 'package:oauth1/oauth1.dart' as auth;
 import 'package:google_fonts/google_fonts.dart';
+import 'home.dart' as home;
 //import 'package:url_launcher/url_launcher.dart';
 //import 'dart:convert';
 
@@ -113,6 +16,12 @@ Paragraph/"Standard text": Ubuntu
 
 void main() {
   runApp(const MyApp());
+
+  runApp(
+    const MaterialApp(
+      home: home.Central(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
