29f2b53cab1c63c9bc9d6f851b88ce839251fe90
anon <arisingh430@gmail.com>
2025-06-16 16:35:42 -0500

    Optimized assignment fetching

---
+++ lib/class_essentials/assignment.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'assignment.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class AssignmentAdapter extends TypeAdapter<Assignment> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Assignment read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Assignment(
+      title: fields[0] as String,
+      type: fields[2] as String,
+      dueDate: fields[1] as DateTime?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Assignment obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.dueDate)
+      ..writeByte(2)
+      ..write(obj.type);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AssignmentAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
